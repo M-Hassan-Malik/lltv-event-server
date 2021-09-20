124cfcc9a1e8b34f90a45e3c840b644814add0d4
Daniyal Sohail <mhassan.malik.1997@gmail.com>
2021-09-20 03:55:40 -0700

    check

--- models/index.js
+++ models/index.js
@@ -3,13 +3,13 @@ const mongoose = require("mongoose");
 //  "mongodb+srv://admin:<password>@cluster0.9oejs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 //  "mongodb://localhost:27017/sales"
 
-mongoose.connect(
+mongoose
+  .connect(
     "mongodb+srv://eventdb:pass1234@cluster0.bd5or.mongodb.net/eventdb",
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
-      maxPoolSize: 10,
       // socketTimeoutMS: 35000,
       // connectTimeoutMS: 10000,
     }
